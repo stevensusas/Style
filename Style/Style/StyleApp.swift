88a3fbe1dd82e15000a6041a07d1d72e296e0d4c
stevensusas <hanqisu@sas.upenn.edu>
2024-12-01 14:03:36 -0500

    restructure

---
+++ Style/Style/StyleApp.swift
@@ -0,0 +1,17 @@
+//
+//  StyleApp.swift
+//  Style
+//
+//  Created by Steven Su on 12/1/24.
+//
+
+import SwiftUI
+
+@main
+struct StyleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Style/StyleTests/StyleTests.swift
@@ -0,0 +1,17 @@
+//
+//  StyleTests.swift
+//  StyleTests
+//
+//  Created by Steven Su on 12/1/24.
+//
+
+import Testing
+@testable import Style
+
+struct StyleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
